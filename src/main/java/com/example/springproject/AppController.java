b37872bd3d264cb2883a907c8fc5a8ed8d156777
Valerianjones1 <dinkiev.valery@yandex.ru>
2023-12-11 16:58:05 +0300

    fix: Fixed edit gruz

--- src/main/java/com/example/springproject/AppController.java
+++ src/main/java/com/example/springproject/AppController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -17,7 +14,7 @@ public class AppController {
     @Autowired
     private GruzService service;
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String viewHomePage(Model model, @Param("keyword") String keyword) {
         List<Gruz> listGruzs = service.listAll(keyword);
         model.addAttribute("listGruzs", listGruzs);
@@ -25,20 +22,20 @@ public class AppController {
         return "index";
     }
 
-    @RequestMapping("/new")
+    @GetMapping("/new")
     public String showNewGruzForm(Model model) {
         Gruz gruz = new Gruz();
         model.addAttribute("gruz", gruz);
         return "new_gruz";
     }
 
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @PostMapping("/save")
     public String saveGruz(@ModelAttribute("gruz") Gruz gruz){
         service.save(gruz);
         return "redirect:/";
     }
 
-    @RequestMapping("/edit/{id}")
+    @GetMapping("/edit/{id}")
     public ModelAndView showEditGruzForm(@PathVariable(name = "id") Long id) {
         ModelAndView mav = new ModelAndView("edit_gruz");
         Gruz gruz = service.get(id);
--- src/main/java/com/example/springproject/GruzRepository.java
+++ src/main/java/com/example/springproject/GruzRepository.java
@@ -1,11 +1,16 @@
 package com.example.springproject;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface GruzRepository extends JpaRepository<Gruz, Long> {
-    @Query("SELECT p FROM Gruz p WHERE CONCAT(p.gruzName,'', p.gruzInsides, '', p.departureDate, '', p.cityArrival, '', p.arrivalDate) LIKE %?1%")
+    @Query("SELECT p FROM Gruz p WHERE CONCAT(p.gruzName,'', p.gruzInsides, '', p.cityDeparture, '',p.departureDate, '', p.cityArrival, '', p.arrivalDate) LIKE %?1%")
     List<Gruz> search(String keyword);
+
+    List<Gruz> findAll(Sort sort);
+
+
 }
